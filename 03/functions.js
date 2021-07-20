745975d732ded71befaf1b10167d1f484cfdaf56
AtharvaPanegai <panegai.atharv@gmail.com>
2021-07-20 18:00:45 +0530

    Functions

--- 03/functions.js
+++ 03/functions.js
@@ -2,12 +2,15 @@
 //     console.log(`This is ${season} it is very hot `);
 // }
 
-// weather("summer");
+
+// weather("Summer");
+
 
 function areaOfRect(l,b){
     return l*b;
 }
 
-var area = areaOfRect(10,5);
-// console.log(area);
-console.log(areaOfRect(10,5));
+ var area= areaOfRect(10,5);
+
+//  console.log(area);
+ console.log(areaOfRect(10,5));
