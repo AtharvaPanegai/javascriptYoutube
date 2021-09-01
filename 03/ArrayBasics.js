1ad8d33c0c8007b6ece31704e1942cc591c7162f
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-01 16:09:57 +0530

    Arrays and Loops Video

---
+++ 03/ArrayBasics.js
@@ -0,0 +1,19 @@
+// old way
+
+var solarSystem = ["Earth","Mercury","Mars","venus"];
+
+// new Way
+
+var countries = new Array("India","England","Japan","Canada");
+
+// console.log(countries[2]);
+
+countries[1] = "USA";
+
+// console.log(countries);
+
+console.log(countries.length);
+
+console.log(countries.indexOf("China"));
+
+console.log(Array.from("Atharva"));
