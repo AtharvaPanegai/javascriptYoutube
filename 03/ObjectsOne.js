fd877529792a26361062145d352a982246ebf0f5
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-01 17:27:07 +0530

    Objects One

---
+++ 03/ObjectsOne.js
@@ -0,0 +1,16 @@
+// syntax
+
+var mobile = {
+    mobileName : "Samsung",
+    numberofCamera : 3,
+    colorofCover : "blue",
+    isMobileYours : true,
+}
+
+
+console.log(mobile.mobileName);
+
+// console.log(mobile["numberofCamera"]);
+console.log(mobile.numberofCamera);
+mobile.numberofCamera = 12;
+console.log(mobile.numberofCamera);
