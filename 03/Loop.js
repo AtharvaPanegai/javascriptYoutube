f95044cec37e6c2e42abf009d20ac1940468f68f
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-01 21:03:52 +0530

    Loops

---
+++ 03/Loop.js
@@ -0,0 +1,19 @@
+/** @format */
+
+// for(let i = 0;i<101;i++){
+//     console.log(i);
+// }
+
+var names = ["Atharva", "Mayur", "Shardul", "Abhi", "DJ", "yash", "harsh"];
+
+// for(let i = 0; i <names.length; i= i+2){
+//     console.log(names[i]);
+// }
+
+// while & do while
+
+var a = 100;
+
+do {
+    console.log("This is do while loop");
+} while (a < 10);
